fca2fb5e98fddbea00d240e6fa2f92ba5f90f96f
loser <loser>
2020-02-23 20:35:50 +0530

    add getter and setter for fields in Person class

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -14,4 +14,36 @@ public class Person {
         this.contactNumber = contactNumber;
         this.email = email;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Long> getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(ArrayList<Long> contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
