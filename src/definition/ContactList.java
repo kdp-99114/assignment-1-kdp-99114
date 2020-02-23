d6a5d62452856e419ec430a494cbec817a5e1d03
loser <loser>
2020-02-23 20:53:14 +0530

    implement the ContactListADT interface

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -1,4 +1,25 @@
 package definition;
 
-public class ContactList {
+import adt.ContactListADT;
+
+public class ContactList implements ContactListADT {
+    @Override
+    public void add(Person person) {
+
+    }
+
+    @Override
+    public void viewContact() {
+
+    }
+
+    @Override
+    public int searchContact(String firstName) {
+        return 0;
+    }
+
+    @Override
+    public Person remove(int index) {
+        return null;
+    }
 }
