ab92695d8ad4fd36c4be5e7fbb50033bcf5615ca
loser <loser>
2020-02-24 19:23:59 +0530

    create menu for user

--- src/execution/ContactListApp.java
+++ src/execution/ContactListApp.java
@@ -8,7 +8,12 @@ public class ContactListApp {
 
         int choice = 0;
         while (choice != 5) {
-
+            System.out.println("Welcome to KDP's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
         }
     }
 }
