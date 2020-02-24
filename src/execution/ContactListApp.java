d9048676c4f36b5d2b920dfa89ddae9b029c315d
loser <loser>
2020-02-24 19:22:52 +0530

    add while loop in main method

--- src/execution/ContactListApp.java
+++ src/execution/ContactListApp.java
@@ -1,7 +1,14 @@
 package execution;
 
+import java.util.Scanner;
+
 public class ContactListApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int choice = 0;
+        while (choice != 5) {
+
+        }
     }
 }
